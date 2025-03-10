939980c7945c0a53fc18d26ddde095989cb39224
maximjsx <maximjsx@users.noreply.github.com>
2025-03-10 20:03:09 +0000

    deploy: 398e07f980c19e1d283681098d4195df86c2335e

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.maximde.hologramlib"},{"l":"com.maximde.hologramlib.bstats"},{"l":"com.maximde.hologramlib.hologram"},{"l":"com.maximde.hologramlib.utils"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.maximde.hologramlib"},{"l":"com.maximde.hologramlib.bstats"},{"l":"com.maximde.hologramlib.hologram"},{"l":"com.maximde.hologramlib.persistence"},{"l":"com.maximde.hologramlib.utils"}];updateSearchResults();
